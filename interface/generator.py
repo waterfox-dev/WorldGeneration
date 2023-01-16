76524c48a4b847da6c5f72eddc6c15ca9c5e2603
waterfox-dev <waterfox80@gmail.com>
2023-01-16 17:44:24 +0100

    Add first config

--- interface/generator.py
+++ interface/generator.py
@@ -4,5 +4,11 @@ class MainWindow :
     
     def __init__(self) -> None : 
         
-        self.win = tk.Tk() 
+        self.win = tk.Tk()
+        self.win.geometry("960x540")
+        self.win.title("World Generator")
+        
+    def show(self) -> None :
+        self.win.mainloop()
+        
         
